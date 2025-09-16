dfcb1fdbeb423c4767a2404d499abf9f69c66139
Esemanraro <128330979+Esemanraro@users.noreply.github.com>
2025-09-16 09:26:31 -0500

    Allow optional emails for admin-created users

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False, index=True)
-    email = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True, nullable=True, index=True)
     password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     # Force password reset on first login or when set by admin
